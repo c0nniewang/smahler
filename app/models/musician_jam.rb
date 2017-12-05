eb7c1d80eb675fc0e4fbc4407899d5c437ac2a53
c0nniewang <connie.wang303@gmail.com>
2017-12-05 14:16:48 -0500

    testing seed data

--- app/models/musician_jam.rb
+++ app/models/musician_jam.rb
@@ -1,4 +1,4 @@
 class MusicianJam < ApplicationRecord
-  belongs_to :jamsession
-  belongs_to :musican, class_name: "User"
+  belongs_to :jam_session
+  belongs_to :musician, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :jamsessions
+  has_many :jam_sessions
   has_many :musician_instruments
   has_many :instruments, through: :musician_instruments
 
