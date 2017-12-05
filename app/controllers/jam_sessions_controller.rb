53f889a9f4e037d7c9d7e0e8c35be585215e0fd3
c0nniewang <connie.wang303@gmail.com>
2017-12-05 17:55:55 -0500

    Refined jam session form

--- app/controllers/jam_sessions_controller.rb
+++ app/controllers/jam_sessions_controller.rb
@@ -1,2 +1,31 @@
 class JamSessionsController < ApplicationController
+  def index
+    @jamsessions = JamSession.all
+  end
+
+  def show
+    @jamsession = JamSession.find(params[:id])
+  end
+
+  def new
+    @jamsession = JamSession.new
+    @instruments = Instrument.all
+  end
+
+  def create
+    byebug
+    @jamsession = JamSession.create(jamsession_params)
+    @instruments = params[:instruments]
+    # CREATE INSTRUMENT JAMS
+
+    redirect_to jam_session_path(@jamsession)
+  end
+
+
+  private
+
+  def jamsession_params
+    params.require(:jam_session).permit(:title, :datetime, :description, :city_id, :genre_id, :host_id)
+  end
+
 end
--- app/models/jam_session.rb
+++ app/models/jam_session.rb
@@ -5,5 +5,29 @@ class JamSession < ApplicationRecord
   has_many :musician_jams
   has_many :musicians, through: :musician_jams
 
+  def city_name=(name)
+    self.city = City.find_or_create_by(name: name)
+  end
+
+  def city_name
+    if self.city == nil
+      return nil
+    else
+    self.city.name
+    end
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    if self.genre == nil
+      return nil
+    else
+    self.genre.name
+    end
+  end
+
 
 end
