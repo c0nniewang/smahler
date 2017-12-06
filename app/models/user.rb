d5a4fe7dc8540c76d4b287705da9e9352c4bb9b2
github username <github email address>
2017-12-06 08:09:51 -0500

    no real changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :jam_sessions
-  has_many :musician_instruments
+  has_many :musician_instruments, foreign_key: "musician_id"
   has_many :instruments, through: :musician_instruments
 
 end
